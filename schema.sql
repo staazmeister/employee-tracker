33e9c864b9aa91018c09a57e7f4a7a6a8c652fdc
Jo Hill <jos.a.hill@gmail.com>
2022-03-07 22:54:32 -0800

    added select/from to schema

--- schema.sql
+++ schema.sql
@@ -5,27 +5,31 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     manager_id INT,
     role_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
+
+SELECT * FROM departments;
+SELECT * FROM roles;
+SELECT * FROM employees;
