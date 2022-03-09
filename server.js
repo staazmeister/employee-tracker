1d13cacf358daf1e4cf29e0a8dd4949a258bc420
Jo Hill <jos.a.hill@gmail.com>
2022-03-08 20:53:32 -0800

    included employee add function

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const cTable = require('console.table');
+const Employee = require(__dirname + '/classes/Employee.js');
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -103,3 +104,139 @@ function viewEmployees() {
 
 }
 //ADD FUNCTIONS//
+
+function addEmployee() {
+    const rolesData = [];
+    const rolesNames = [];
+
+    const employeesData = [];
+    const employeesNames = ['No Manager'];
+
+    getRolesAsync()
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
+                rolesData.push(data[i]);
+                rolesNames.push(data[i].role)
+            }
+
+            getEmployeesAsync()
+                .then(data => {
+                    for (let i = 0; i < data.length; i++) {
+                        employeesData.push(data[i]);
+                        employeesNames.push(data[i].last_name)
+                    }
+                }).catch(err => {
+                    console.log(err);
+                })
+        }).catch(err => {
+            console.log(err);
+        });
+
+    inquirer.prompt([{
+            type: 'input',
+            name: 'firstName',
+            message: `What is the employee's first name?`,
+            default: () => {},
+            validate: firstName => {
+                let valid = /^[a-zA-Z0-9 ]{1,30}$/.test(firstName);
+                if (valid) {
+                    return true;
+                } else {
+                    console.log(`. Your name must be between 1 and 30 characters.`)
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: `What is the employee's last name?`,
+            default: () => {},
+            validate: lastName => {
+                let valid = /^[a-zA-Z0-9 ]{1,30}$/.test(lastName);
+                if (valid) {
+                    return true;
+                } else {
+                    console.log(`. Your name must be between 1 and 30 characters.`)
+                    return false;
+                }
+            }
+
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: `What is the employee's role?`,
+            choices: rolesNames
+        },
+        {
+            type: 'list',
+            name: 'manager',
+            message: `Who is the employee's manager?`,
+            choices: employeesNames
+        }
+    ]).then(answers => {
+        let roleId;
+        let managerId;
+
+        for (let i = 0; i < rolesData.length; i++) {
+            if (answers.role === rolesData[i].role) {
+                roleId = rolesData[i].id;
+            }
+        }
+
+        for (let i = 0; i < employeesData.length; i++) {
+            if (answers.manager === employeesData[i].last_name) {
+                managerId = employeesData[i].id;
+            } else if (answers.manager === 'No Manager') {
+                managerId = null;
+            }
+        }
+        insertEmployee(answers.firstName, answers.lastName, roleId, managerId);
+    });
+}
+
+function insertEmployee(firstName, lastName, roleId, managerId) {
+    connection.query('INSERT INTO employees SET ?', new Employee(firstName, lastName, roleId, managerId), (err, res) => {
+        if (err) throw err;
+        console.log(`Successfully added ${firstName} ${lastName} to Employees`);
+        init();
+    });
+}
+
+// ===============
+// Async Functions
+// ===============
+
+function getRolesAsync() {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT id, title AS 'role' FROM roles ORDER BY role`, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(data);
+        });
+    });
+}
+
+function getEmployeesAsync() {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT id, last_name FROM employees ORDER BY last_name`, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(data);
+        });
+    });
+}
+
+function getDepartmentsAsync() {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT * FROM departments`, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(data);
+        })
+    })
+}
