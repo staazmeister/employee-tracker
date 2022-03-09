01622ac0434a8324ebe4b1aea522850b6d63ac18
Jo Hill <jos.a.hill@gmail.com>
2022-03-08 20:14:02 -0800

    add ids for all sections

--- seeds.sql
+++ seeds.sql
@@ -1,25 +1,25 @@
 USE employee_tracker_db;
 
-INSERT INTO departments(name)
-VALUES('Sales'),
-('Marketing'),
-('HR');
+INSERT INTO departments(id,name)
+VALUES(1,'Sales'),
+(2,'Marketing'),
+(3,'HR');
 
-INSERT INTO roles(title, salary, department_id)
-VALUES('Sales Leader', 75000.00, 1),
-('Junior Salesman', 60000.00, 1),
-('Marketing Leader', 90000.00, 2),
-('Junior Associate', 55000.00, 2),
-('HR Manager', 95000.00, 3),
-('Secretary', 50000.00, 3);
+INSERT INTO roles(id, title, salary, department_id)
+VALUES(1,'Sales Leader', 75000.00, 1),
+(2,'Junior Salesman', 60000.00, 1),
+(3,'Marketing Leader', 90000.00, 2),
+(4,'Junior Associate', 55000.00, 2),
+(5,'HR Manager', 95000.00, 3),
+(6,'Secretary', 50000.00, 3);
 
-INSERT INTO employees(first_name, last_name, role_id, manager_id)
-VALUES('Mickey', 'Mouse', 1, NULL),
-('Donald', 'Duck', 2, 1),
-('Bruno', 'Madrigal', 2, 1),
-('Chicken', 'Little', 3, NULL),
-('Doofus', 'Drake', 4, 4),
-('Fa', 'Zhou', 4, 4),
-('Figaro', 'Cat', 5, NULL),
-('Gladstone', 'Gander', 6, 7),
-('Heihei', 'Rooster', 6, 7);
+INSERT INTO employees(id,first_name, last_name, role_id, manager_id)
+VALUES(1,'Mickey', 'Mouse', 1, NULL),
+(2,'Donald', 'Duck', 2, 1),
+(3,'Bruno', 'Madrigal', 2, 1),
+(4,'Chicken', 'Little', 3, NULL),
+(5,'Doofus', 'Drake', 4, 4),
+(6,'Fa', 'Zhou', 4, 4),
+(7,'Figaro', 'Cat', 5, NULL),
+(8,'Gladstone', 'Gander', 6, 7),
+(9,'Heihei', 'Rooster', 6, 7);
