96543c5a93415616153de630d2ac6afa152a4e70
Jo Hill <jos.a.hill@gmail.com>
2022-03-08 17:13:20 -0800

    added view functions

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: '',
+    password: 'password',
     database: "employee_tracker_db"
 });
 
@@ -61,4 +61,31 @@ function startPrompt() {
 
         }
     })
+}
+//VIEW FUNCTIONS//
+function viewAllEmployees() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+        function(err, res) {
+            if (err) throw err
+            console.table(res)
+            startPrompt()
+        })
+}
+
+function viewAllRoles() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;",
+        function(err, res) {
+            if (err) throw err
+            console.table(res)
+            startPrompt()
+        })
+}
+
+function viewAllDepartments() {
+    connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;",
+        function(err, res) {
+            if (err) throw err
+            console.table(res)
+            startPrompt()
+        })
 }
