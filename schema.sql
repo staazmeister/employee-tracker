768409a5eb38c315e019ba1c7d0f0b933de07207
Jo Hill <jos.a.hill@gmail.com>
2022-03-08 18:50:07 -0800

    revised schema.sql

--- schema.sql
+++ schema.sql
@@ -4,32 +4,25 @@ CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
-
-CREATE TABLE departments (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+CREATE TABLE departments(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL
 );
 
-
-CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
+CREATE TABLE roles(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(11, 2) NOT NULL,
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-
-CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+CREATE TABLE employees(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
     manager_id INT,
-    role_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
-
-SELECT * FROM departments;
-SELECT * FROM roles;
-SELECT * FROM employees;
